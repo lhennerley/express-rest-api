9d56565bde5fa1bd470bae6323d0bd0562042247
Luke Hennerley <lhennerley@sidetrade.com>
2020-04-07 16:16:51 +0100

    Add Webpack to repository for deployment purposes

--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -2,6 +2,11 @@ import { Router } from "express";
 import cors from "cors";
 import parser from "body-parser";
 import compression from "compression";
+import morgan from "morgan";
+import helmet from "helmet";
+
+export const handleLogging = (router: Router) => router.use(morgan("combined"));
+export const handleSecurity = (router: Router) => router.use(helmet());
 
 export const handleCors = (router: Router) =>
   router.use(cors({ credentials: true, origin: true }));
